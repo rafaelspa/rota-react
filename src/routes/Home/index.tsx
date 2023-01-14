8ac8d75217337e22f239b7c9fea95a99447318ba
rafaelspa <rsdepaula67@gmail.com>
2023-01-14 09:30:55 -0300

    Home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,26 @@
+import "./styles.css";
+import { NavLink } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import homeIcon from "../../assets/home.svg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <header>
+        <div className="rr-home-navigation-container">
+          <div className="rr-home-links">
+            <NavLink to="/home">Início</NavLink>
+            <NavLink to="/products">Produtos</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </div>
+          <div className="rr-home-icon-container">
+            <Link to="/">
+              <img src={homeIcon} alt="Home" />
+            </Link>
+          </div>
+        </div>
+      </header>
       <Outlet />
     </>
   );
